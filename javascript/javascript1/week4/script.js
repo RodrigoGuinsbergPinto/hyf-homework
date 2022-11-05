a259fc6332b90a7269878625ac4bed3a671a39e4
rodrigoguinsberg <rodrigoguinsberg@gmail.com>
2022-11-05 16:20:39 +0100

    File modified after review and feedback

--- javascript/javascript1/week4/script.js
+++ javascript/javascript1/week4/script.js
@@ -2,7 +2,7 @@ let userName = ""; // Rodrigo;
 const todoList = [];
 let lettersToCount = [];
 
-function getReply(command) {
+function getName(command) {
   if (command == " " || typeof command !== "string") {
     alert(`command is not a string`);
   }
@@ -23,20 +23,16 @@ function getReply(command) {
   } else if (command.includes("What is my name")) {
     console.log(`Your name is ${userName}`);
   }
+}
+getName("Hello my name is Rodrigo");
+// getName("Hello my name is Rodrigo");
+getName("What is my name?");
 
+function addToDo(command) {
   if (command.includes("Add") && command.includes("to my todo")) {
     const myTodo = command.slice(4, -11);
     todoList.push(myTodo);
     console.log(`${myTodo} added to your todo`);
-  }
-
-  if (command.includes("Remove")) {
-    const removeFromTodo = command.slice(7, -13);
-    for (let i = 0; i < todoList.length; i++) {
-      if (todoList[i] === removeFromTodo) {
-        todoList.splice(i, 1);
-      }
-    }
   }
 
   if (command === "What is on my todo?") {
@@ -44,7 +40,28 @@ function getReply(command) {
       console.log(todoList[i]);
     }
   }
-  if (command === "What day is it today?") {
+}
+addToDo("Add fishing to my todo");
+addToDo("Add singing in the shower to my todo");
+
+// console.log(todoList);
+addToDo("What is on my todo?");
+
+function removeToDo(toDo) {
+  if (toDo.includes("Remove")) {
+    const removeFromTodo = toDo.slice(7, -13);
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i] === removeFromTodo) {
+        todoList.splice(i, 1);
+      }
+    }
+  }
+}
+removeToDo("Remove fishing from my todo");
+// console.log(todoList);
+
+function whatDayIsItToday(date) {
+  if (date === "What day is it today?") {
     const today = new Date();
     const day = today.toLocaleString("en-US", { day: "2-digit" });
     const month = today.toLocaleString("en-US", { month: "long" });
@@ -52,36 +69,24 @@ function getReply(command) {
     const date = `${day}. of ${month} ${year}`;
     console.log(date);
   }
+}
+whatDayIsItToday("What day is it today?");
 
-  if (command.includes("What is") && command.includes("+")) {
-    const regex = /\d+/g;
-    const result = command.match(regex);
-    console.log(Number(result[0]) + Number(result[1]));
-  } else if (command.includes("What is") && command.includes("-")) {
-    const regex = /\d+/g;
-    const result = command.match(regex);
-    console.log(Number(result[0]) - Number(result[1]));
-  } else if (command.includes("What is") && command.includes("*")) {
-    const regex = /\d+/g;
-    const result = command.match(regex);
-    console.log(Number(result[0]) * Number(result[1]));
-  } else if (command.includes("What is") && command.includes("/")) {
-    const regex = /\d+/g;
-    const result = command.match(regex);
-    console.log(Number(result[0]) / Number(result[1]));
-  }
-
-  if (command.includes("Set a timer for")) {
+function setATimeFor(time) {
+  if (time.includes("Set a timer for")) {
     const regexTime = /\d+/;
-    const timeToBeSet = command.match(regexTime);
+    const timeToBeSet = time.match(regexTime);
     console.log(`Timer set for ${timeToBeSet} minutes. ⌛`);
     setTimeout(function () {
       console.log(`Time done.`);
     }, timeToBeSet * 60000);
   }
+}
+// setATimeFor("Set a timer for 1 minutes?");
 
-  if (command.includes("How many letters")) {
-    const words = command.split(" ");
+function countLetters(sentence) {
+  if (sentence.includes("How many letters")) {
+    const words = sentence.split(" ");
     for (let i = 7; i < words.length; i++) {
       const capturedWords = words[i];
       lettersToCount.push(capturedWords);
@@ -92,19 +97,22 @@ function getReply(command) {
     console.log(`There are "${totalLetters}" letters in your sentence.`);
   }
 }
+countLetters("How many letters are in the sentence: What is your name?");
 
-getReply("Hello my name is Rodrigo");
-// getReply("Hello my name is Rodrigo");
-getReply("What is my name?");
-getReply("Add fishing to my todo");
-getReply("Add singing in the shower to my todo");
-// getReply("Remove fishing from my todo");
-// console.log(todoList);
-getReply("What is on my todo?");
-getReply("What day is it today?");
-getReply("What is 2 + 2?");
-getReply("What is 7 - 2?");
-getReply("What is 7 * 7");
-getReply("What is 30 / 3?");
-// getReply("Set a timer for 2 minutes?");
-getReply("How many letters are in the sentence: What is your name?");
+function getCalc(command) {
+  if (
+    (command.includes("What is") && command.includes("+")) ||
+    command.includes("-") ||
+    command.includes("*") ||
+    command.includes("/")
+  ) {
+    const nonLetters = /[^A-Za-z?]/g;
+    const myResult = command.match(nonLetters);
+
+    console.log(eval(myResult.join("")));
+  }
+}
+getCalc("What is 3 + 2?");
+// getCalc("What is 7 - 3?");
+// getCalc("What is 7 * 7?");
+// getCalc("What is 30 / 3?");
