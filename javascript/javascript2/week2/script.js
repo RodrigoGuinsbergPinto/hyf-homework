bef8db4800a4bc6d78900ffa9278b35271e31f41
rodrigoguinsberg <rodrigoguinsberg@gmail.com>
2022-11-05 14:12:16 +0100

    Homework JS2-week2 Exercise 1 with correct files

--- javascript/javascript2/week2/script.js
+++ javascript/javascript2/week2/script.js
@@ -1,66 +1,99 @@
-// FIND THE SHORTEST WORD
+// WARMUP ARRAY EXERCISES
+// 1.1. Doubling of number
 
-const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
+const numbers = [1, 2, 3, 4];
 
-let shortestWord = "";
+const doubleOddNumbers = function (arrayOfNumbers) {
+  const newArrayOfNumbers = arrayOfNumbers
+    .filter((number) => number % 2 !== 0)
+    .map((number) => number * 2);
+  console.log("The doubled numbers are: ", newArrayOfNumbers);
+};
+doubleOddNumbers(numbers);
 
-function getShortestWord(array) {
-  for (let i = 0; i < array.length; i++) {
-    shortestWord = array[0];
-    if (array[i].length < shortestWord.length) {
-      shortestWord = array[i];
-      return shortestWord;
-    }
+// 1.2. Working with movies
+// 1-short title
+const shortTitleMovies = movies.filter((movie) => movie.title.length <= 5);
+console.log(shortTitleMovies);
+
+// 2-long titles
+const longTitleMovies = movies.filter((movie) => movie.title.length > 50);
+console.log(longTitleMovies);
+
+// 3-Count the number of movies made between 1980-1989
+const the80sMovies = movies.filter(
+  (movie) => movie.year >= 1980 && movie.year <= 1989
+).length;
+console.log(the80sMovies); // 638
+
+// 4-Create a new array that has an extra key called tag. The tag is based on the rating: Good (>= 7), Average (>= 4 and < 7), Bad (< 4)
+const moviesWithTag = movies.filter((movie) => {
+  if (movie.rating >= 7) {
+    return (movie.tag = "Good");
   }
-}
-
-console.log(getShortestWord(danishWords));
-
-// FIND AND COUNT THE DANISH LETTERS
-const danishString = "Jeg har en blå bil";
-const danishString2 = "Blå grød med rød bær";
-const danishString3 = "";
-
-let countÆ = 0;
-let countØ = 0;
-let countÅ = 0;
-// let totalDanishLetters = countÆ + countØ + countÅ;
-
-function findDanishLetters(sentence) {
-  const lettersArray = sentence.toLowerCase().split("");
-
-  for (let i = 0; i < lettersArray.length; i++) {
-    if (lettersArray[i] === "æ") {
-      countÆ++;
-    } else if (lettersArray[i] === "ø") {
-      countØ++;
-    } else if (lettersArray[i] === "å") {
-      countÅ++;
-    }
-  }
-  return countÆ + countØ + countÅ;
-}
-
-function displayDanishLettersFound() {
-  const danishLetters = {};
-
-  if (countÆ + countØ + countÅ === 0) {
-    console.log(`No danish letters found!`); // Implementation for string without Danish letters or empty string.
+  if (movie.rating >= 4) {
+    return (movie.tag = "Average");
   } else {
-    danishLetters.total = countÆ + countØ + countÅ;
-    if (countÆ > 0) {
-      danishLetters.æ = countÆ;
-    }
-    if (countØ > 0) {
-      danishLetters.ø = countØ;
-    }
-    if (countÅ > 0) {
-      danishLetters.å = countÅ;
-    }
-    console.log(danishLetters);
+    return (movie.tag = "Bad");
   }
-}
+});
+// Using Ternary Operator
+// let rating =
+//     (movie.rating >= 7) ? "Good" : movie.rating >= 4 && movie.rating < 7 ? "Average" : "Bad";
+//   movie.tag = rating;
+//   return movie;
+// });
+console.log(moviesWithTag);
 
-findDanishLetters(danishString2);
+// 5-Movies rated higher than 6
+const movieRating = movies
+  .filter((movie) => movie.rating > 6)
+  .map((movie) => movie.rating);
+console.log(movieRating);
 
-displayDanishLettersFound(findDanishLetters);
+// 6-Count the total number of movies containing any of following keywords:
+// Surfer, Alien or Benjamin. So if there were 3 movies that contained
+//Surfer, 1 with Alien and 2 with Benjamin, you would return 6.
+// Can you make sure the search is case insensitive?
+
+const keywordsRegex = /Surfer|Alien|Benjamin/gi;
+
+const keywords = movies.filter((movie) => {
+  let findKeywords = movie.title.match(keywordsRegex);
+  return findKeywords;
+}).length;
+console.log(
+  `The total number of movies containing the words Surfer, Alien or Benjamin is ${keywords}.`
+);
+
+// 8-Calculate the average rating of all the movies
+
+const averageRating = movies.reduce(function (acc, movie) {
+  const average = acc + movie.rating / movies.length;
+  return average;
+}, 0);
+
+console.log(averageRating);
+
+// 9-Count the total number of Good, Average and Bad movies using reduce.
+const moviesByTag1 = movies.reduce(
+  (mv, movie) => {
+    if (movie.tag == "Good") {
+      mv.goodMovies++;
+    } else if (movie.tag == "Average") {
+      mv.averageMovies++;
+    } else if (movie.tag == "Bad") {
+      mv.badMovies++;
+    }
+    return mv;
+  },
+  { goodMovies: 0, averageMovies: 0, badMovies: 0 }
+);
+
+const moviesByTag2 = moviesWithTag.reduce((acc, movie) => {
+  return { ...acc, [movie.tag]: (acc[movie.tag] || 0) + 1 };
+}, {});
+
+console.log(moviesByTag1);
+
+console.log(moviesByTag2);
