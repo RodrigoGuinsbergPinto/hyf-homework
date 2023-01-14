75ad319ffd154eed9443519ba2fc711787efd86f
rodrigoguinsberg <rodrigoguinsberg@gmail.com>
2023-01-14 22:35:25 +0100

    Database-week 1 homework with changes after review

--- databases/week1/lesson1_homework.sql
+++ databases/week1/lesson1_homework.sql
@@ -10,6 +10,7 @@ WHERE due_date IS NULL;
 -- 3. Find all the tasks that are marked as done
 SELECT *
 FROM task
+INNER JOIN status ON task.status_id = status.id
 WHERE status_id = 3;
 
 -- 4. Find all the tasks that are not marked as done
@@ -32,20 +33,19 @@ LIMIT 1;
 -- 7. Get the title and due date of all tasks where the title or description contains database
 SELECT title, due_date
 FROM task
-WHERE title REGEXP 'database' OR description REGEXP 'database';
+WHERE title LIKE '%database%' OR description LIKE '%database%';
 
 -- 8. Get the title and status (as text) of all tasks
 SELECT task.title, status.name AS status_description
 FROM task
-INNER JOIN status
+INNER JOIN status ON task.status_id = status.id
 ORDER BY status_description;
 
 -- 9. Get the name of each status, along with a count of how many tasks have that status
-SELECT status.name, COUNT(task.status_id) AS 'number_of_task'
+SELECT status.name, COUNT(task.id) AS 'number_of_task'
 FROM task
-INNER JOIN status
-ON task.status_id = status.id
-GROUP BY task.status_id;
+INNER JOIN status ON task.status_id = status.id
+GROUP BY status.name;
 
 -- 10. Get the names of all statuses, sorted by the status with most tasks first
 SELECT status.name, COUNT(task.status_id) AS 'number_of_task'
