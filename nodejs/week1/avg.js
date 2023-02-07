b4f26ff959d2a5dc1db950a7eb3d41dc555ba290
rodrigoguinsberg <rodrigoguinsberg@gmail.com>
2023-02-07 08:45:42 +0100

    Replacing reserved word arguments to args

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,7 +1,6 @@
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-const average =
-  arguments.reduce((acc, val) => acc + parseInt(val), 0) / arguments.length;
+const average = args.reduce((acc, val) => acc + parseInt(val), 0) / args.length;
 
 if (isNaN(average)) {
   console.log("Only numbers are accepted as arguments.");
